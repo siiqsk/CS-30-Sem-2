7274a56f71f7c5998b6b8cc771f048762d39e7e1
sophi <sophi@localhost>
2026-03-06 11:10:15 -0700

    .

---
+++ Chapter10/src/Mastery/BreakAPlate.java
@@ -0,0 +1,129 @@
+/*
+
+Program: BreakAPlate.java          Last Date of this Revision: March 5th, 2026
+
+Purpose: The Break-A-Plate game allows a player to try to break all plates. 
+		 If all three plates are broken, a first prize is awarded, otherwise,
+		 a consolation prize is awarded.
+
+Author: Sophia Chen 
+School: CHHS
+Course: CompSci-30
+ 
+
+*/
+
+package Mastery;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class BreakAPlate implements ActionListener {
+	
+	private ImageIcon plate, broken1, broken2, broken3, tiger, sticker;
+	private JLabel plate1, plate2, plate3, status, prize;
+	private JFrame frmBreakaplate;
+	private JButton button;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BreakAPlate window = new BreakAPlate();
+					window.frmBreakaplate.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public BreakAPlate() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		
+		plate = new ImageIcon("/Chapter10/src/Mastery/plate.jpg");
+		broken1 = new ImageIcon("../Chapter10/src/skillbuilder/die2.gif");
+		broken2 = new ImageIcon("../Chapter10/src/skillbuilder/die3.gif");
+		broken3 = new ImageIcon("../Chapter10/src/skillbuilder/die4.gif");
+		tiger = new ImageIcon("../Chapter10/src/skillbuilder/die5.gif");
+		sticker = new ImageIcon("../Chapter10/src/skillbuilder/die6.gif");
+		
+		frmBreakaplate = new JFrame();
+		frmBreakaplate.setTitle("BreakAPlate");
+		frmBreakaplate.setBounds(100, 100, 400, 299);
+		frmBreakaplate.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(254, 255, 255));
+		frmBreakaplate.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		plate1 = new JLabel(plate);
+		plate1.setBounds(37, 34, 80, 80);
+		panel.add(plate1);
+		
+		plate2 = new JLabel(plate);
+		plate2.setBounds(158, 34, 80, 80);
+		panel.add(plate2);
+		
+		plate3 = new JLabel(plate);
+		plate3.setBounds(279, 34, 80, 80);
+		panel.add(plate3);
+		
+		status = new JLabel("New label");
+		status.setBounds(165, 163, 61, 16);
+		panel.add(status);
+		
+		prize = new JLabel("");
+		prize.setBounds(158, 177, 80, 80);
+		panel.add(prize);
+		
+		button = new JButton("Play");
+		button.setBackground(new Color(254, 127, 125));
+		button.setBounds(138, 126, 117, 29);
+		
+		button.setActionCommand("poll");
+		button.addActionListener(this);
+		panel.add(button);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		String eventName = e.getActionCommand();
+		if(eventName.equals("play")) {
+			
+			plate1.setIcon(plate);
+			plate2.setIcon(plate);
+			plate3.setIcon(plate);
+			
+			button.setText("Stop");
+			
+			
+			
+	}
+		
+	}
+
+}
--- Chapter10/src/skillbuilder/Roll.java
+++ Chapter10/src/skillbuilder/Roll.java
@@ -2,11 +2,31 @@ package skillbuilder;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Component;
 
-public class Roll {
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
+
+public class Roll implements ActionListener{
 
 	private JFrame frame;
+	JButton rollButton;
+	JLabel Dice1;
+	JLabel Dice2;
+	ImageIcon D1;
+	ImageIcon D2;
+	ImageIcon D3;
+	ImageIcon D4;
+	ImageIcon D5;
+	ImageIcon D6;
 
 	/**
 	 * Launch the application.
@@ -36,8 +56,77 @@ public class Roll {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setForeground(new Color(255, 227, 250));
+		frame.setBounds(100, 100, 323, 220);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 227, 250));
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		Dice1 = new JLabel("");
+		Dice1.setBounds(56, 28, 98, 98);
+		panel.add(Dice1);
+		
+		Dice2 = new JLabel("");
+		Dice2.setBounds(177, 28, 98, 98);
+		panel.add(Dice2);
+		
+		rollButton = new JButton("Roll Die");
+		rollButton.setBackground(new Color(222, 228, 250));
+		rollButton.setFont(new Font("PT Serif", Font.PLAIN, 13));
+		
+		rollButton.setActionCommand("roll");
+		rollButton.addActionListener(this);
+		
+		rollButton.setBounds(103, 138, 117, 29);
+		panel.add(rollButton);
+		
+		D1 = new ImageIcon("../Chapter10/src/skillbuilder/die1.gif");
+		D2 = new ImageIcon("../Chapter10/src/skillbuilder/die2.gif");
+		D3 = new ImageIcon("../Chapter10/src/skillbuilder/die3.gif");
+		D4 = new ImageIcon("../Chapter10/src/skillbuilder/die4.gif");
+		D5 = new ImageIcon("../Chapter10/src/skillbuilder/die5.gif");
+		D6 = new ImageIcon("../Chapter10/src/skillbuilder/die6.gif");
+		
+		}	
+		
+		public void actionPerformed(ActionEvent e) {
+			
+			String eventName = e.getActionCommand();
+			if(eventName.equals("roll")) {
+			
+				int Roll1 = (int)(6*Math.random() + 1);
+				int Roll2 = (int)(6*Math.random() + 1);
+				
+				switch (Roll1) {
+				
+				case 1: Dice1.setIcon(D1);break;
+				case 2: Dice1.setIcon(D2);break;
+				case 3: Dice1.setIcon(D3);break;
+				case 4: Dice1.setIcon(D4);break;
+				case 5: Dice1.setIcon(D5);break;
+				case 6: Dice1.setIcon(D6);break;
+					
+				}
+				
+				switch (Roll2) {
+				
+				case 1: Dice2.setIcon(D1);break;
+				case 2: Dice2.setIcon(D2);break;
+				case 3: Dice2.setIcon(D3);break;
+				case 4: Dice2.setIcon(D4);break;
+				case 5: Dice2.setIcon(D5);break;
+				case 6: Dice2.setIcon(D6);break;
+					
+				}
+				
+		}
+	
+		
+		
+		
+		
 	}
-
 }
