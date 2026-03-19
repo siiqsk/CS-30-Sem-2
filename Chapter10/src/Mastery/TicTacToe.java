7090f9976fd38a394caa2378f4580a9b17c05602
sophi <sophi@localhost>
2026-03-18 21:53:12 -0600

    .

--- Chapter10/src/Mastery/TicTacToe.java
+++ Chapter10/src/Mastery/TicTacToe.java
@@ -8,6 +8,8 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -21,11 +23,11 @@ public class TicTacToe implements ActionListener {
 
 	private JFrame frmTictactoe;
 	JLabel catPic;
-	private ImageIcon cat, catDance, cat2;
+	private ImageIcon cat, catDance, cat2, hamster;
 	private JPanel mainPanel, boardPanel, gamePanel;
-	JButton playButton, humanButton, compButton, catMode;
+	JButton playButton, humanButton, compButton, catMode, resetButton;
 	JButton space[][] = new JButton[3][3];
-	String player1, player2, gameMode, currPlayer, boxNum;
+	String player1, player2, gameMode, currPlayer, boxNum, humanSpot, computerSpot;
 	int movesMade;
 	String winner = " ";
 	
@@ -33,6 +35,7 @@ public class TicTacToe implements ActionListener {
 	Boolean catOn = false;
 	
 	private JLabel playerTurn, turnStat;
+	Timer timer = new Timer();
 
 	
 	public void board() {
@@ -42,7 +45,6 @@ public class TicTacToe implements ActionListener {
 			for(int t = 0; t< space[0].length; t++) {
 			
 			space[i][t] = new JButton(" ");
-			//space[i][t].setIcon(cat);
 			space[i][t].setFocusPainted(false);
 			space[i][t].setForeground(new Color(109, 68, 90));
 			space[i][t].setActionCommand("box" + i+t);
@@ -77,10 +79,6 @@ public class TicTacToe implements ActionListener {
 	public TicTacToe() {
 		initialize();
 		board();
-		
-		while (gameOn == true) {
-			winner();
-		}
 	}
 
 	/**
@@ -91,6 +89,7 @@ public class TicTacToe implements ActionListener {
 		cat2 = new ImageIcon("../Chapter10/src/Mastery/tofu (1).jpeg");
 		cat = new ImageIcon("../Chapter10/src/Mastery/cat1.jpeg");
 		catDance = new ImageIcon("../Chapter10/src/Mastery/catdance.gif");
+		hamster = new ImageIcon("../Chapter10/src/Mastery/hamster.gif");
 		
 		frmTictactoe = new JFrame();
 		frmTictactoe.setTitle("TicTacToe");
@@ -111,7 +110,7 @@ public class TicTacToe implements ActionListener {
 		
 		playButton = new JButton("⟡ PLAY ⟡");
 		playButton.setForeground(new Color(109, 68, 90));
-		playButton.setFont(new Font("Yuanti SC", Font.BOLD, 26));
+		playButton.setFont(new Font("Silom", Font.BOLD, 26));
 		playButton.setFocusPainted(false);
 		playButton.setBounds(379, 243, 205, 69);
 		playButton.setActionCommand("play");
@@ -122,6 +121,7 @@ public class TicTacToe implements ActionListener {
 		humanButton.setFont(new Font("Skia", Font.BOLD, 16));
 		humanButton.setFocusPainted(false);
 		humanButton.setForeground(new Color(180, 102, 139));
+		humanButton.setToolTipText("Player Vs Player");
 		humanButton.setBounds(379, 200, 100, 39);
 		humanButton.setActionCommand("human");
 		gameMode = ("human");
@@ -132,13 +132,14 @@ public class TicTacToe implements ActionListener {
 		compButton.setFont(new Font("Skia", Font.BOLD, 13));
 		compButton.setFocusPainted(false);
 		compButton.setForeground(new Color(247, 221, 237));
+		compButton.setToolTipText("Player Vs Computer");
 		compButton.setBounds(476, 200, 108, 39);
 		compButton.setActionCommand("computer");
 		compButton.addActionListener(this);
 		mainPanel.add(compButton);
 		
 		catMode = new JButton("cat mode ⌞ x ⌝");
-		catMode.setFont(new Font("LingWai TC", Font.PLAIN, 16));
+		catMode.setFont(new Font("Gurmukhi Sangam MN", Font.PLAIN, 14));
 		catMode.setFocusPainted(false);
 		catMode.setForeground(new Color(109, 67, 90));
 		catMode.setToolTipText("Change the background of the board!");
@@ -148,28 +149,41 @@ public class TicTacToe implements ActionListener {
 		mainPanel.add(catMode);
 		
 		catPic = new JLabel("");
-		catPic.setBounds(403, 24, 154, 172);
+		catPic.setIcon(hamster);
+		catPic.setBounds(412, 24, 158, 172);
 		mainPanel.add(catPic);
 		
 		gamePanel = new JPanel();
 		gamePanel.setBorder(new LineBorder(new Color(199, 144, 173), 3, true));
 		gamePanel.setBackground(new Color(254, 255, 255));
-		gamePanel.setBounds(390, 152, 194, 120);
+		gamePanel.setBounds(390, 152, 194, 99);
 		gamePanel.setVisible(false);
 		mainPanel.add(gamePanel);
 		gamePanel.setLayout(null);
 		
 		playerTurn = new JLabel("current turn:");
 		playerTurn.setForeground(new Color(128, 86, 106));
-		playerTurn.setFont(new Font("Yuanti SC", Font.PLAIN, 19));
+		playerTurn.setFont(new Font("Skia", Font.PLAIN, 17));
 		playerTurn.setBounds(45, 6, 108, 39);
 		playerTurn.setVisible(false);
 		gamePanel.add(playerTurn);
 		
 		turnStat = new JLabel("");
-		turnStat.setBounds(19, 46, 156, 55);
+		turnStat.setForeground(new Color(198, 144, 172));
+		turnStat.setFont(new Font("Silom", Font.PLAIN, 29));
+		turnStat.setBounds(28, 27, 160, 58);
 		turnStat.setVisible(false);
 		gamePanel.add(turnStat);
+		
+		resetButton = new JButton("reset");
+		resetButton.setFont(new Font("Monaco", Font.PLAIN, 13));
+		resetButton.setForeground(new Color(198, 144, 172));
+		resetButton.setBounds(448, 108, 88, 32);
+		resetButton.setVisible(false);
+		resetButton.setFocusPainted(false);
+		resetButton.setActionCommand("reset");
+		resetButton.addActionListener(this);
+		mainPanel.add(resetButton);
 	
 	}
 	
@@ -178,6 +192,36 @@ public void actionPerformed(ActionEvent e) {
 		String eventName = e.getActionCommand();
 		
 		System.out.println(eventName);
+		
+		if(eventName.equals("reset")) {
+			
+			for (int i = 0; i < space.length; i++) {
+				
+				for(int t = 0; t< space[0].length; t++) {
+					
+					boardPanel.remove(space[i][t]);
+					
+				}
+			}
+			
+			board();
+			
+			gameOn = false;
+			
+			playButton.setVisible(true);
+			humanButton.setVisible(true);
+			compButton.setVisible(true);
+			catMode.setVisible(true);
+			catPic.setVisible(true);
+			
+			playerTurn.setVisible(false);
+			turnStat.setVisible(false);
+			gamePanel.setVisible(false);
+			resetButton.setVisible(false);
+			
+			winner = " ";
+			
+		}
 		
 		if (gameOn == false) {
 			
@@ -201,12 +245,11 @@ public void actionPerformed(ActionEvent e) {
 			else if (eventName.equals("catModeOff")) {
 				catOn = false;
 				catMode.setActionCommand("catModeOn");
-				catPic.setIcon(null);
+				catPic.setIcon(hamster);
 				catMode.setText("cat mode ⌞ x ⌝");
 			}
 			
 			if(eventName.equals("play")) {
-				playButton.setActionCommand("stop");
 				gameOn=true;
 				
 				playButton.setVisible(false);
@@ -218,92 +261,124 @@ public void actionPerformed(ActionEvent e) {
 				playerTurn.setVisible(true);
 				turnStat.setVisible(true);
 				gamePanel.setVisible(true);
-				//mainPanel.revalidate();
-				//mainPanel.repaint();
+				resetButton.setVisible(true);
 				
 				movesMade = 0 ;
 				player1 = "x";
 				player2 = "o";
 				currPlayer = player1;
+				turnStat.setText("player: " + currPlayer);
 			}
 		}	
 		
-		if (gameOn == true && eventName.contains("box")) {
+		if (gameOn == true) {
 			
-			if (gameMode.equals("human")){
+			if (eventName.contains("box")) {
+		
 				
-				boxNum = eventName;
-				moves("x","o");
+				if (gameMode.equals("human")){
+					
+					humanSpot = eventName;
+					moves(humanSpot, humanSpot);
+					
+				}
+				
+				if(gameMode.equals("computer")) {
+					
+					humanSpot = eventName;
+					moves(humanSpot, computerSpot);
+					
+					if (winner.equals(" ") && turnStat.getText().equals("player: " + player2)) {
+						
+						turnStat.setText("computer");
+				    
+					}
+				}
+			}
+			
+			if ((currPlayer == player2) && gameMode.equals("computer") && winner.equals(" ")){
+				
+				timer.schedule(new TimerTask() {
+				    public void run() {
+		
+				    	computerTurn();
+				    	moves(humanSpot, computerSpot);
+
+				    }
+				    
+				}, 800);
+				
+				
 				
 			}
+		}
+	}
 
-			    /* (movesMade <= 9 /*&& winner()==" ") {
-
-			        int row = eventName.charAt(3) - '0';
-			        int col = eventName.charAt(4) - '0';
-
-			        System.out.println(row);
-			        System.out.println(col);
-			        System.out.println("hi");
-
-			        if (space[row][col].getText().equals(" ")) {
-
-			            space[row][col].setText(currPlayer);
-			            movesMade++;
-
-			            if (currPlayer.equals(player1)) {
-			            	
-			                currPlayer = player2;
-			                
-			            } else {
-			            	
-			                currPlayer = player1;
-			                
-			            }
-			        }
-		
-			    } */
-			
-			    }}
-
-
-private void moves(String p1, String p2) {
+private void computerTurn() {
 	
-	movesMade++;
-	winner();
-	System.out.println(winner);
+	while (currPlayer == player2) {
+		
+		int i = (int)(3*Math.random()); 
+		int t = (int)(3*Math.random()); 
+		
+		System.out.println(i + "and" + t);
+		
+		computerSpot = "box" + i + t;
+		
+		if ((space[i][t]).getText() == (" ")) {
+			
+			break;
+		}
+	}
+}
+
+private void moves(String spotP1, String spotP2) {
+	
 	
 	
 	if ((movesMade <= 9) && (winner == " ")) {
 		
-		player1 = p1;
-		player2 = p2;
+		player1 = "x";
+		player2 = "o";
+		
+		if (currPlayer == player1) {
+			
+			boxNum = spotP1;
+		}
+		else {
+			
+			boxNum = spotP2;
+			
+		}
+		
+		
 
-        int row = boxNum.charAt(3) - '0';
-        int col = boxNum.charAt(4) - '0';
+        int i = boxNum.charAt(3) - '0';
+        int t = boxNum.charAt(4) - '0';
 
-        System.out.println(row);
-        System.out.println(col);
-        System.out.println("hi");
+        System.out.println(i);
+        System.out.println(t);
 
-        if ((space[row][col].getText().equals(" "))) /*&& (space[row][col].getIcon().equals(null)))*/ {
+        if ((space[i][t].getText().equals(" "))){
+        	
+        	movesMade++;
         	
         	if (catOn == false) {
         		
-        		space[row][col].setFont(new Font("Yuanti SC", Font.BOLD, 45));
-        		space[row][col].setText(currPlayer);
+        		space[i][t].setFont(new Font("Yuanti SC", Font.BOLD, 45));
+        		space[i][t].setText(currPlayer);
    
         		
             } else {
             	
             	if (currPlayer == player1) {
-            		space[row][col].setText(currPlayer);
-            		space[row][col].setIcon(cat);
+            		space[i][t].setText(currPlayer);
+            		space[i][t].setIcon(cat);
             		
             	}else {
             		
-            		space[row][col].setText(currPlayer);
-            		space[row][col].setIcon(cat2);}
+            		space[i][t].setText(currPlayer);
+            		space[i][t].setIcon(cat2);}
           
             }
         	
@@ -319,18 +394,20 @@ private void moves(String p1, String p2) {
                 currPlayer = player1;
                 
             }
+            
+            turnStat.setText("player: " + currPlayer);
         }
         
    
 
     }
+	
+	
+	winner();
+	System.out.println(winner);
+	
 }
 private void winner() {
-	
-	/*if (movesMade == 9) {
-		System.out.println("yay");
-		winner = "play";
-	}*/
 	
 	for (int i = 0; i < space.length; i ++) {
 		
@@ -338,13 +415,64 @@ private void winner() {
 		(space[i][1].getText()).equals ((space[i][2]).getText()) &&
 		!((space[i][0].getText().equals(" ")))) {
 			
-		winner = ((space[i][0]).getText()) + "Is WINNER";
+		winner = ((space[i][0]).getText()) + " WINS ! ";
+		
 		//return winner;
 		}
 		
 		}
+	
+	for (int t = 0; t < space[0].length; t ++) {
+		
+		if(((space[0][t]).getText()).equals((space[1][t]).getText()) &&
+		(space[1][t].getText()).equals ((space[2][t]).getText()) &&
+		!((space[0][t].getText().equals(" ")))) {
+			
+		winner = ((space[0][t]).getText()) + " WINS ! ";}
+		//return winner;
+
+	}
+		
+	if	(((space[0][0]).getText()).equals((space[1][1]).getText()) &&
+		(space[1][1].getText()).equals ((space[2][2]).getText()) &&
+		!((space[0][0].getText().equals(" ")))) {
+			
+		winner = ((space[0][0]).getText()) + " WINS ! ";}
+		//return winner;
+
+
+	if	(((space[0][2]).getText()).equals((space[1][1]).getText()) &&
+		(space[1][1].getText()).equals ((space[2][0]).getText()) &&
+		!((space[0][2].getText().equals(" ")))) {
+			
+		winner = ((space[0][2]).getText()) + " WINS ! ";}
+		//return winner;
+	
+	if ((movesMade == 9) && (winner.equals(" "))){
+		
+		winner = ("  DRAW !");
+		
+	}
+	
+	if ((winner.equals(player2 + " WINS ! ")) && gameMode.equals("computer")) {
+		
+		winner = "CP WINS !";
+		
+	}
+	
+	if (winner != " ") {
+		
+		playerTurn.setVisible(false);
+		turnStat.setText(winner);
+		turnStat.setFont(new Font("Silom", Font.PLAIN, 30));
+							
 	}
 	
 	
+	
+	}
 }
+	
+	
+
 
